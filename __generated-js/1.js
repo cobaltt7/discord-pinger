fb968791dc23ca80fcdb274f066769474ca18db4
RedGuy12 <RedGuy12@users.noreply.github.com>
2022-04-24 03:17:07 +0000

    Generate workflows
    Signed-off-by: RedGuy12 <RedGuy12@users.noreply.github.com>

--- __generated-js/1.js
+++ __generated-js/1.js
@@ -30,8 +30,9 @@ Discord.once("ready", async () => {
 const tokenIndex=	process.argv.findIndex(e=>e==="token:")+1
 Discord.login(tokenIndex?process.argv[tokenIndex]:process.env.BOT_TOKEN);
 
-async function error(user) {
-	return await	(await Discord.users.fetch("914999467286093844")).send({
+function error(user) {
+	return async (error) => {
+		(await Discord.users.fetch("914999467286093844")).send({
 			content:
 				"ERROR:\n```js\n" +
 				JSON.stringify(error) +
@@ -52,4 +53,6 @@ async function error(user) {
 				"`\nIndex: " +
 				"1",
 		});
+		return undefined;
+	};
 }
